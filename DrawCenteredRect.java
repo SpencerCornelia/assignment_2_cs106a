bc91a71f491511def70921ed1f43340d2df102df
S <s>
2017-03-05 12:32:38 -0800

    {"types":"C","runs":3}

--- DrawCenteredRect.java
+++ DrawCenteredRect.java
@@ -15,7 +15,7 @@ public class DrawCenteredRect extends GraphicsProgram {
 	/** Size of the centered rect */
 	private static final int WIDTH = 350;
 	private static final int HEIGHT = 270;
-	private int x = 0;
+	private int x = 50%;
 	private int y = 0;
 
 	public void run() {
