74c0b65da448f6e6d00e3ecb81067ac2f4dfb773
S <s>
2017-03-05 15:57:29 -0800

    {"types":"C","runs":42}

--- FindRange.java
+++ FindRange.java
@@ -7,8 +7,11 @@
  */
 
 import acm.program.*;
+import java.util.Scanner;
 
 public class FindRange extends ConsoleProgram {
+	int static final SENTINAL = readString("Please enter a value: ");
+	
 	public void run() {
 		println("This program finds the largest and smallest numbers.");
 	}
