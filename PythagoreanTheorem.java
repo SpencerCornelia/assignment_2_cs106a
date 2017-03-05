c1d70b3cf8fa41079042eced954f4eb71441fe8c
S <s>
2017-03-05 15:57:49 -0800

    {"types":"C","runs":42}

--- PythagoreanTheorem.java
+++ PythagoreanTheorem.java
@@ -16,6 +16,7 @@ public class PythagoreanTheorem extends ConsoleProgram {
 		System.out.println("Enter values to compute the pythagorean theorem: ");
 		System.out.println("a: ");
 		double userInputA = a.nextDouble();
+		System.out.println("b: ");
 		double userInputB = b.nextDouble();
 		System.out.println("a: " + userInputA);
 		System.out.println("b: " + userInputB);
