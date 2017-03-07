a52c224452a5b28e975af7802d1abf70688dac85
S <s>
2017-03-06 21:53:48 -0800

    {"types":"C","runs":46}

--- FindRange.java
+++ FindRange.java
@@ -15,12 +15,29 @@ public class FindRange extends ConsoleProgram {
 		Scanner input = new Scanner(System.in);
 		println("This program finds the largest and smallest numbers.");
 		int userInput = 1;
+		//initialize an array
 		
 		while (userInput != sentinal) {
 			System.out.print("Please enter a value: ");
 			userInput = input.nextInt();
+			//push userInput into the array
 		}
+		smallestNumber();
+		largestNumber();
 		input.close();
+	}
+	
+	private int smallestNumber() {
+		//sort the array
+		//if array.length = 1 return the value
+		//if array.length = 0 return an error notifying user they need to input an integer
+	}
+	
+	private int largestNumber() {
+		//sort the array
+		//if array.length = 1 return nothing
+		//if array.length = 0 return nothing
+		//return array[array.length - 1]
 	}
 }
 
