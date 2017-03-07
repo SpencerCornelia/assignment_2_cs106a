5310c0fdff68f4ef2c9da11f4f0fe97eb3c21aa1
S <s>
2017-03-06 22:02:14 -0800

    {"types":"C","runs":46}

--- Hailstone.java
+++ Hailstone.java
@@ -10,9 +10,28 @@ import acm.program.*;
 import java.util.Scanner;
 
 public class Hailstone extends ConsoleProgram {
+	int counter;
+	int value;
 	public void run() {
 		Scanner a = new Scanner(System.in);
 		System.out.println("Please enter a number: ");
+		int userInput = a.nextInt();
+		counter = userInput;
+		runHailstone();
+	}
+	
+	public void runHailstone() {
+		while (counter != 0) {
+			if (counter % 2 != 0) {
+				value = (counter * 3) + 1;
+				println(counter + " is odd so I make 3n + 1: " + value);
+				counter = value;
+			} else {
+				value = (counter / 2);
+				println(counter + " is even so I take half: " + value);
+				counter = value;
+			}
+		}
 	}
 }
 
